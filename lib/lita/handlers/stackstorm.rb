59d7ad936c58e17913de42e9b6b177e5da15e9c5
Hannes Schaller <hannes.schaller@anovis.com>
2017-08-11 15:59:25 +0200

    hopefully fix st2 api

--- lib/lita/handlers/stackstorm.rb
+++ lib/lita/handlers/stackstorm.rb
@@ -1,15 +1,14 @@
 require 'json'
 require 'net/http'
 require 'yaml'
+require 'pry'
 
 class Array
-  def swap!(a,b)
+  def swap!(a, b)
     self[a], self[b] = self[b], self[a]
     self
   end
 end
-
-
 
 module Lita
   module Handlers
@@ -21,7 +20,7 @@ module Lita
       config :password, required: true
       config :auth_port, required: false, default: 9100
       config :execution_port, required: false, default: 9101
-      config :emoji_icon, required: false, default: ":panda:"
+      config :emoji_icon, required: false, default: ':panda:'
 
       on :connected, :stream_listen
 
@@ -29,13 +28,14 @@ module Lita
         attr_accessor :token, :expires
       end
 
-      def self.config(config)
+      def self.config(_config)
         self.token = nil
         self.expires = nil
       end
 
-      route /^st2 login$/, :login, command: false, help: { "st2 login" => "login with st2-api" }
-      route /^st2 (ls|aliases|list)$/, :list, command: false, help: { "st2 list" => "list available st2 chatops commands" }
+      route /^st2 login$/, :login, command: false, help: { 'st2 login' => 'login with st2-api' }
+      route /^st2 (ls|aliases|list)$/, :list, command: false, help: { 'st2 list' => 'list available st2 chatops commands' }
+      route /^st2 connect$/, :stream_listen, command: false, help: { 'st2 connect' => 'connect to eventstream' }
 
       route /^!(.*)$/, :call_alias, command: false, help: {}
 
@@ -43,7 +43,7 @@ module Lita
         case robot.config.robot.adapter
         when :slack
           payload = {
-            action: "slack.chat.postMessage",
+            action: 'slack.chat.postMessage',
             parameters: {
               username: user,
               text: message,
@@ -51,45 +51,45 @@ module Lita
               channel: channel
             }
           }
-          make_post_request("/executions", payload)
+          make_post_request('/executions', payload)
         else
           target = Lita::Source.new(user: user, room: Lita::Room.fuzzy_find(channel))
           robot.send_message(target, message)
         end
       end
 
-      def stream_listen(payload)
-        if expired
-          authenticate
-        end
-        uri = URI("#{url_builder()}/stream")
-        Thread.new {
-          Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+      def stream_listen(_payload)
+        authenticate if expired
+        uri = URI("#{url_builder}/stream")
+        log.debug "ST2 connecting"
+        Thread.new do
+          Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
             request = Net::HTTP::Get.new uri
-            request['X-Auth-Token'] = headers()['X-Auth-Token']
+            request['X-Auth-Token'] = headers['X-Auth-Token']
             request['Content-Type'] = 'application/x-yaml'
             http.request request do |response|
               io = StringIO.new
+              log.debug "ST2 connected"
               response.read_body do |chunk|
                 c = chunk.strip
-                if c.length > 0
+                unless c.empty?
                   io.write chunk
-                  event = YAML.load(io.string)
+                  event = YAML.safe_load(io.string)
                   if event['event'] =~ /st2\.announcement/
                     direct_post(event['data']['payload']['channel'],
                                 event['data']['payload']['message'],
                                 event['data']['payload']['user'])
                   end
-                  io.reopen("")
+                  io.reopen('')
                 end
               end
             end
           end
-        }
+        end
       end
 
       def auth_builder
-        if Integer(config.auth_port) == 443 and config.url.start_with?('https')
+        if (Integer(config.auth_port) == 443) && config.url.start_with?('https')
           "#{config.url}/auth"
         else
           "#{config.url}:#{config.auth_port}/v1"
@@ -97,7 +97,7 @@ module Lita
       end
 
       def url_builder
-        if Integer(config.execution_port) == 443 and config.url.start_with?('https')
+        if (Integer(config.execution_port) == 443) && config.url.start_with?('https')
           "#{config.url}/api"
         else
           "#{config.url}:#{config.execution_port}/v1"
@@ -105,7 +105,7 @@ module Lita
       end
 
       def authenticate
-        resp = http.post("#{auth_builder()}/tokens") do |req|
+        resp = http.post("#{auth_builder}/tokens") do |req|
           req.body = {}
           req.headers['Authorization'] = http.set_authorization_header(:basic_auth, config.username, config.password)
         end
@@ -115,14 +115,12 @@ module Lita
       end
 
       def call_alias(msg)
-        if expired
-          authenticate
-        end
+        authenticate if expired
         command = msg.matches.flatten.first
-        found = ""
+        found = ''
         redis.scan_each do |a|
           possible = /#{a}/.match(command)
-          if not possible.nil?
+          unless possible.nil?
             found = a
             break
           end
@@ -137,7 +135,7 @@ module Lita
           source_channel: 'chatops',
           notification_channel: 'lita'
         }
-        s = make_post_request("/aliasexecution", payload)
+        s = make_post_request('/aliasexecution', payload)
         j = JSON.parse(s.body)
         if s.success?
           msg.reply "Got it! Details available at #{config.url}/#/history/#{j['execution']['id']}/general"
@@ -147,28 +145,49 @@ module Lita
       end
 
       def list(msg)
-        if expired
-          authenticate
-        end
-        redis.keys.each {|k| redis.del k }
-        s = make_request("/actionalias", "")
+        authenticate if expired
+        redis.keys.each { |k| redis.del k }
+        s = make_request('/actionalias', '')
         if JSON.parse(s.body).empty?
-          msg.reply "No Action Aliases Registered"
+          msg.reply 'No Action Aliases Registered'
         else
           j = JSON.parse(s.body)
-          a = ""
-          extra_params = '(\\s+(\\S+)\\s*=("([\\s\\S]*?)"|\'([\\s\\S]*?)\'|({[\\s\\S]*?})|(\\S+))\\s*)*'
-          j.take_while{|i| i['enabled'] }.each do |command|
+          a = ''
+          j.take_while { |i| i['enabled'] }.each do |command|
+            # convert each representation to a regular expression
             command['formats'].each do |format|
-              f = format.gsub(/(\s*){{\s*\S+\s*=\s*(?:({.+?}|.+?))\s*}}(\s*)/, '\\s*([\\S]+)?\\s*')
-              f = f.gsub(/\s*{{.+?}}\s*/, '\\s*([\\S]+?)\\s*')
-              f = "^\\s*#{f}#{extra_params}\\s*$"
-              redis.set(f, {format: format, object: command}.to_json)
-              a+= "#{format} -> #{command['description']}\n"
+              if format.is_a? Hash
+                if format.key?('representation')
+                  format['representation'].each do |rep|
+                    f = convert_to_regex(rep)
+                    redis.set(f, { format: rep, object: command }.to_json)
+                    a += "#{format['display']} -> #{command['description']}\n"
+                  end
+                else
+                  f = convert_to_regex(format['display'])
+                  redis.set(f, { format: format['display'], object: command }.to_json)
+                  a += "#{format['display']} -> #{command['description']}\n"
+                end
+
+              elsif format.is_a? String
+                f = convert_to_regex(format)
+                redis.set(f, { format: format, object: command }.to_json)
+                a += "#{format} -> #{command['description']}\n"
+              else
+                next
+              end
             end
           end
           msg.reply a
         end
+      end
+
+      def convert_to_regex(format)
+        extra_params = '(\\s+(\\S+)\\s*=("([\\s\\S]*?)"|\'([\\s\\S]*?)\'|({[\\s\\S]*?})|(\\S+))\\s*)*'
+        f = format.gsub(/(\s*){{\s*\S+\s*=\s*(?:({.+?}|.+?))\s*}}(\s*)/, '\\s*([\\S]+)?\\s*')
+        f = f.gsub(/\s*{{.+?}}\s*/, '\\s*([\\S]+?)\\s*')
+        f = "^\\s*#{f}#{extra_params}\\s*$"
+        f
       end
 
       def login(msg)
@@ -187,15 +206,15 @@ module Lita
       end
 
       def make_request(path, body)
-        resp = http.get("#{url_builder()}#{path}") do |req|
+        resp = http.get("#{url_builder}#{path}") do |req|
           req.headers = headers
-          req.body = body.to_json if not body.empty?
+          req.body = body.to_json unless body.empty?
         end
         resp
       end
 
       def make_post_request(path, body)
-        resp = http.post("#{url_builder()}#{path}") do |req|
+        resp = http.post("#{url_builder}#{path}") do |req|
           req.body = {}
           req.headers = headers
           req.body = body.to_json
@@ -203,11 +222,10 @@ module Lita
         resp
       end
 
-
       def headers
         headers = {}
         headers['Content-Type'] = 'application/json'
-        headers['X-Auth-Token'] = "#{self.class.token}"
+        headers['X-Auth-Token'] = self.class.token.to_s
         headers
       end
 
